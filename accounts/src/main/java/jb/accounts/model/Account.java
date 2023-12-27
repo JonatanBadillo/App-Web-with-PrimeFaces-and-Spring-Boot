410ac7a9cf8ec640f92d71453403da7df0599e8e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-12-27 14:34:03 -0600

    Creating entity class of Account

---
+++ accounts/src/main/java/jb/accounts/model/Account.java
@@ -0,0 +1,23 @@
+package jb.accounts.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idAccount;
+    String name;
+    String typeAccount;
+    Double balance;
+}
