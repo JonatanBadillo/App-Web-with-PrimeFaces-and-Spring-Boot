7ad2e9b26272cf764d068bcd4ab9626ed757f10d
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-01-15 18:27:03 -0600

    Adding with PrimeFaces

--- accounts/src/main/java/jb/accounts/controller/IndexController.java
+++ accounts/src/main/java/jb/accounts/controller/IndexController.java
@@ -1,10 +1,13 @@
 package jb.accounts.controller;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import jb.accounts.model.Account;
 import jb.accounts.service.AccountService;
 import lombok.Data;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,4 +40,16 @@ public class IndexController {
         this.selectedAccount = new Account();
     }
 
+    public void saveAccount(){
+        logger.info("Account to save: " + this.selectedAccount);
+        if(this.selectedAccount.getIdAccount() == null){
+            this.accountService.saveAccount(this.selectedAccount);
+            this.accounts.add(this.selectedAccount);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Added Account"));
+        }
+        // Hiding the window
+        PrimeFaces.current().executeScript("PF('windowModalAccount').hide()");
+        // Update the table
+        PrimeFaces.current().ajax().update("form-accounts:messages","form-accounts:accounts-table");
+    }
 }
